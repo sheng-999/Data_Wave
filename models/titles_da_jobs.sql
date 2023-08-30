5c2a99b9099b4e02ec4efe4e5d91f68daac93f86
sabri75malo@gmail.com <sabri75malo@gmail.com>
2023-08-30 15:50:40 +0000

    1eres categories de job_title

---
+++ models/titles_da_jobs.sql
@@ -0,0 +1,24 @@
+
+
+
+CREATE TABLE models.da_titles (
+    id INT PRIMARY KEY,
+    da_title VARCHAR(255)
+);
+
+INSERT INTO models.da_titles (da_title)
+VALUES
+('data analyst', 'data analyst'),
+('data analyst', 'analyste data'),
+('data analyst', 'analyste de donnees');
+('analyste donnees'),
+('analyste des donnees'),
+('data integrity analyst'),
+('data quality analyst'),
+('data quality analyste'),
+('analyste base de donnees'),
+('programme analyst'),
+('economic analysis'),
+('analyste master data'),
+('data-analyst'),
+('data & pricing analyst');
