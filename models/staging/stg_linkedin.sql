7faa903987c92cf2fc72d2504d5b27d474529121
Kyomars Shekohivand <kyomars.lewagon@gmail.com>
2023-08-30 10:05:46 +0000

    linkedin concatenated

--- models/staging/stg_linkedin.sql
+++ models/staging/stg_linkedin.sql
@@ -5,3 +5,24 @@ UNION ALL
 SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_yvelines
 UNION ALL
 SELECT * FROM teamprojectdamarket.raw_data.linkedin_remote_premier_emploi
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_aura
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_bourgogne
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_bretagnep1
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_bretagnep2
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_centre
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_grand_est
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_normandie
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_remote_others
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_remote_premier_emploi
+UNION ALL
+SELECT * FROM teamprojectdamarket.raw_data.linkedin_onsite_paris_premier_emploi
+WHERE whole_desc is not null
